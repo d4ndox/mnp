5ee99bfd757830fa2b15b5317c993ce87aebbde0
nonie <nonie@C4AABFD71236378D286D8BBCC731B654198D63E3D129D1BE776276AD96C73D0AA5F8BACC43BD.tox>
2020-01-31 14:05:03 +0100

    Added command line option »-a --account« to select a Monero account. Output of JSON_RPC version. Removed some debug output.

--- mnp.c
+++ mnp.c
@@ -26,8 +26,10 @@
 #include <string.h>
 #include <errno.h>
 #include "./inih/ini.h"
+#include "./cjson/cJSON.h"
 #include "wallet.h"
 #include "globaldefs.h"
+#include <inttypes.h>
 
 /* verbose is extern @ globaldefs.h. Be noisy.*/
 int verbose = 0;
@@ -35,15 +37,16 @@ int verbose = 0;
 static const struct option options[] = {
 	{"help"         , no_argument      , NULL, 'h'},
         {"rpc_user"     , required_argument, NULL, 'u'},
-        {"rpc_password" , required_argument, NULL, 'a'},
+        {"rpc_password" , required_argument, NULL, 'r'},
         {"rpc_host"     , required_argument, NULL, 'i'},
         {"rpc_port"     , required_argument, NULL, 'p'},
+        {"account"      , required_argument, NULL, 'a'},
         {"version"      , no_argument      , NULL, 'v'},
 	{"verbose"      , no_argument      , &verbose, 1},
 	{NULL, 0, NULL, 0}
 };
 
-static char *optstring = "hu:a:i:p:v";
+static char *optstring = "hu:r:i:p:a:v";
 static void usage(int status);
 static int handler(void *user, const char *section, 
                    const char *name, const char *value);
@@ -51,30 +54,31 @@ static void printmnp(void);
 
 int main(int argc, char **argv)
 {
-    /* Variables are set by getopt and/or config parser handler()*/
+    /* variables are set by getopt and/or config parser handler()*/
     int opt, lindex = -1;
     char *rpc_user = NULL;
     char *rpc_password = NULL;
     char *rpc_host = NULL;
     char *rpc_port = NULL;
     char *url = NULL;
+    char *account = NULL;
     char *answer = NULL;
     int length = 0;
 
-    /* get HOME directory from environment */
+    /* get home directory from environment */
     const char *homedir;
-    if ((homedir = getenv("HOME")) == NULL) {
+    if ((homedir = getenv("home")) == NULL) {
         homedir = getpwuid(getuid())->pw_dir;
     }
     char *ini = NULL;
     char *home = strndup(homedir, MAX_DATA_SIZE);
     asprintf(&ini, "%s/%s", home, CONFIG_FILE);
     free(home);
-    /* parse config INI file */
+    /* parse config ini file */
     struct Config config;
 
     if (ini_parse(ini, handler, &config) < 0) {
-        fprintf(stderr, "Can't load %s. Try make install.\n", ini);
+        fprintf(stderr, "can't load %s. try make install.\n", ini);
         exit(EXIT_FAILURE);
     }
     free(ini);
@@ -89,13 +93,16 @@ int main(int argc, char **argv)
 	    case 'u':
                 rpc_user = strndup(optarg, MAX_DATA_SIZE);
                 break;
-            case 'a':
+            case 'd':
                 rpc_password = strndup(optarg, MAX_DATA_SIZE);
                 break;
             case 'i':
                 rpc_host = strndup(optarg, MAX_DATA_SIZE);
 	    case 'p':
                 rpc_port = strndup(optarg, MAX_DATA_SIZE);
+                break;
+            case 'a':
+                account = strndup(optarg, MAX_DATA_SIZE);
                 break;
             case 'v':
                 printmnp();
@@ -110,8 +117,10 @@ int main(int argc, char **argv)
 
     int ret = 0;
 
-    /* If no command option is set - use the config file */
-    if (rpc_user == NULL) {
+    /* if no command option is set - use the config file */
+    if (account == NULL) {
+        asprintf(&account, "%s", config.mnp_account);
+    } if (rpc_user == NULL) {
         asprintf(&rpc_user, "%s", config.rpc_user);
     } if (rpc_password == NULL) {
         asprintf(&rpc_password, "%s", config.rpc_password);
@@ -126,21 +135,32 @@ int main(int argc, char **argv)
 
     asprintf(&urlport,"http://%s:%s/json_rpc", rpc_host, rpc_port);
     asprintf(&userpwd,"%s:%s", rpc_user, rpc_password);
-    if (DEBUG) fprintf(stderr, "%s\n", urlport);
-    if (DEBUG) fprintf(stderr, "%s\n", userpwd);
-    if (DEBUG) printmnp();
-
-    if (0 > (ret = wallet(urlport, GET_VERSION, userpwd, &answer))) {
-        fprintf(stderr, "Could not connect to host: %s\n", urlport);
-        exit(EXIT_FAILURE);
-    }
 
     if (verbose)
     {
-        fprintf(stderr, "%s\n", GET_VERSION);
-        fprintf(stderr, "%d bytes retrieved\n", ret);
+        fprintf(stdout, "Starting ... \n");
+        printmnp();
+        fprintf(stdout, "Connecting: %s\n", urlport);;
     }
-    fprintf(stdout, "%s", answer);
+
+    if (0 > (ret = wallet(urlport, GET_VERSION, userpwd, &answer))) {
+        fprintf(stderr, "could not connect to host: %s\n", urlport);
+        exit(EXIT_FAILURE);
+    }
+    
+    if (verbose) fprintf(stdout, "%d bytes retrieved\n", ret);
+
+    const cJSON *result = NULL;
+    //const cJSON *version = NULL;
+    const cJSON *j_answer = cJSON_Parse(answer);
+    result = cJSON_GetObjectItem(j_answer, "result");
+    unsigned int version = cJSON_GetObjectItem(result, "version")->valueint;
+    unsigned int major = (version & MAJOR_MASK) >> 16;
+    unsigned int minor = (version & MINOR_MASK);
+    char *jresult = cJSON_Print(result);
+    if (verbose) fprintf(stdout, "rpc version: v%d.%d.\n", major, minor);
+    char *jstr = cJSON_Print(j_answer);
+    if (DEBUG) fprintf(stdout, "%s\n", jstr);
     free(url);
 }
 
@@ -153,6 +173,8 @@ static void usage(int status)
     if (ok)
     fprintf(stdout,
     "Usage: mnp [OPTION]\n\n"
+    "  -a  --account [ACCOUNT]\n"
+    "               Monero account number. 0 = default.\n\n"
     "      --rpc_user [RPC_USER]\n"
     "               rpc user for monero-wallet-rpc.\n"
     "               Needed for authetification @ rpc wallet.\n\n"
@@ -200,6 +222,8 @@ static int handler(void *user, const char *section, const char *name,
         pconfig->mnp_daemon = strndup(value, MAX_DATA_SIZE);
     } else if (MATCH("mnp", "verbose")) {
         pconfig->mnp_verbose = strndup(value, MAX_DATA_SIZE);
+    } else if (MATCH("mnp", "account")) {
+        pconfig->mnp_account = strndup(value, MAX_DATA_SIZE);
     } else {
         return 0;  /* unknown section/name, error */
     }
