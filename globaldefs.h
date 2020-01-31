48c6f7b16f03a2c0f2d6f2d74968874ca00d3de2
nonie <nonie@C4AABFD71236378D286D8BBCC731B654198D63E3D129D1BE776276AD96C73D0AA5F8BACC43BD.tox>
2020-01-31 13:55:58 +0100

    Added GET_HEIGHT, MAJOR and MINOR_MASK used for JSON get_version.

--- globaldefs.h
+++ globaldefs.h
@@ -3,7 +3,7 @@
 
 #define MAX_DATA_SIZE   (2048)
 #define VERSION         "0.0.1"
-#define DEBUG           (1)
+#define DEBUG           (0)
 #define CONFIG_FILE     ".mnp.ini"
 extern int verbose;
 
@@ -13,15 +13,19 @@ struct MemoryStruct {
 };
 
 struct Config {
-    const char* rpc_user;
-    const char* rpc_password;
-    const char* rpc_host;
-    const char* rpc_port;
-    const char* mnp_daemon;
-    const char* mnp_verbose;
+    const char  *rpc_user;
+    const char  *rpc_password;
+    const char  *rpc_host;
+    const char  *rpc_port;
+    const char  *mnp_daemon;
+    const char  *mnp_verbose;
+    const char  *mnp_account;
 };
 
 #define CONTENT_TYPE    "Content-Type: application/json"
 #define GET_VERSION     "{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"get_version\"}" 
+#define GET_HEIGHT      "{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"get_height\"}"
+#define MAJOR_MASK      (0xFFFF0000)
+#define MINOR_MASK      (0x0000FFFF)
 
 #endif
