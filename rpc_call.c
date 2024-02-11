35c5a691efa3e22b611214af42cf8c83b41d6409
nonie <d4ndo@proton.me>
2024-02-11 19:26:23 +0100

    passing a struct instead of 5 parameter.

--- rpc_call.c
+++ rpc_call.c
@@ -24,7 +24,90 @@
 #include "rpc_call.h"
 #include "globaldefs.h"
 
-int rpc_call(const char *method, const char *params, const char *urlport, const char *userpwd, cJSON **rpc_reply)
+
+int rpc_call(struct rpc_wallet *monero_wallet)
+{
+    int ret = 0;
+
+    char *urlport = NULL;
+
+    if (monero_wallet->host != NULL && monero_wallet->port != NULL) {
+        asprintf(&urlport,"http://%s:%s/json_rpc", monero_wallet->host, monero_wallet->port);
+    } else {
+        fprintf(stderr, "rpc_host and/or rpc_port is missing\n");
+        return -1;
+    }
+
+    /* prepare rpc_user and rpc_password to connect to the wallet */
+    char *userpwd = NULL;
+
+    if (monero_wallet->user != NULL && monero_wallet->pwd != NULL) {
+        asprintf(&userpwd,"%s:%s", monero_wallet->user, monero_wallet->pwd);
+    } else {
+        fprintf(stderr, "rpc_user and/or rpc_password is missing\n");
+        return -1;
+    }
+    
+    char *method = helperfunction(monero_wallet->monero_rpc_method);
+
+    /*
+     * Pack the method string into a JSON frame for rpc call.
+     */
+    cJSON *rpc_frame = cJSON_CreateObject();
+
+    if (cJSON_AddStringToObject(rpc_frame, "jsonrpc", JSON_RPC) == NULL) ret = -1;
+    if (cJSON_AddStringToObject(rpc_frame, "id", "0") == NULL) ret = -1;
+    if (cJSON_AddStringToObject(rpc_frame, "method", method) == NULL) ret = -1;
+
+    char *method_call = cJSON_Print(rpc_frame);
+
+    if (method_call == NULL) ret = -1;
+
+    /*
+     * rpc method call send to the wallet
+     */
+    char *reply = NULL;
+    if (0 > (ret = wallet(urlport, method_call, userpwd, &reply))) {
+        fprintf(stderr, "could not connect to host: %s\n", urlport);
+        ret = -1;
+    }
+
+    /*
+     * rpc_reply is the return value of this function
+     * testing for errors while parseing the JSON string.
+     */
+    monero_wallet->reply = cJSON_Parse(reply);
+    if (monero_wallet->reply == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL) {
+            fprintf(stderr, "Error before: %s\n", error_ptr);
+            ret = -1;
+        }
+    }
+
+    /*
+     * Check for error code returned from wallet(rpc) call
+     * test rpc_reply for any error codes.
+     */
+    cJSON *error = cJSON_GetObjectItemCaseSensitive(monero_wallet->reply, "error");
+    const cJSON *mesg = cJSON_GetObjectItemCaseSensitive(error, "message");
+
+    if (error != NULL && mesg->valuestring != NULL) {
+        fprintf(stderr, "error message rpc: %s\n", mesg->valuestring);
+        ret = -1;
+    }
+
+    if (DEBUG) fprintf(stdout, "%d bytes received\n", ret);
+
+    cJSON_Delete(rpc_frame);
+    cJSON_Delete(error);
+    free(method);
+    free(method_call);
+    free(reply);
+    return ret;
+}
+
+int rpc_call2(const char *method, const char *params, const char *urlport, const char *userpwd, cJSON **rpc_reply)
 {
     int ret = 0;
 
@@ -75,11 +158,32 @@ int rpc_call(const char *method, const char *params, const char *urlport, const 
         ret = -1;
     }
 
-    if (verbose) fprintf(stdout, "%d bytes received\n", ret);
+    if (DEBUG) fprintf(stdout, "%d bytes received\n", ret);
 
     cJSON_Delete(rpc_frame);
     cJSON_Delete(error);
     free(method_call);
     free(reply);
     return ret;
+}
+
+
+char* helperfunction(enum monero_rpc_method method)
+{
+    char *mtd = NULL;
+
+    switch (method) {
+        case GET_VERSION:
+            asprintf(&mtd, "%s", GET_VERSION_CMD);
+                break;
+        case GET_HEIGHT:
+            asprintf(&mtd, "%s", GET_HEIGHT_CMD);
+                break;
+        case GET_BALANCE:
+                break;
+        default:
+                break;
+
+    }        
+    return mtd;
 }
--- rpc_call.h
+++ rpc_call.h
@@ -3,6 +3,32 @@
 
 #include "./cjson/cJSON.h"
 
-int rpc_call(const char *method, const char *params, const char *urlport, const char *userpwd, cJSON **reply);
+enum monero_rpc_method {
+    GET_VERSION,
+    GET_HEIGHT,
+    GET_BALANCE,
+    END_RPC_SIZE
+};
+
+struct rpc_wallet {
+       int monero_rpc_method;
+       char *params;
+       char *account;
+       char *host;
+       char *port;
+       char *user;
+       char *pwd;
+       cJSON *reply;
+};
+ 
+//for ( int i = 0; i != END_RPC_SIZE; i++ )
+//{
+//   monero_rpc_method monero_method = static_cast<monero_rpc_type>(i);
+//}
+
+
+//int rpc_call2(const char *method, const char *params, const char *urlport, const char *userpwd, cJSON **reply);
+int rpc_call(struct rpc_wallet *monero_wallet);
+char* helperfunction(enum monero_rpc_method method);
 
 #endif
