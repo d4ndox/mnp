81b6f26311cd20bd9db7338a18668cfc31b56859
d4ndox <d4ndo@proton.me>
2024-02-16 17:55:48 +0100

    Changed date and gpg-key in comment.

--- rpc_call.c
+++ rpc_call.c
@@ -1,18 +1,17 @@
-/*
- * Monero Named Pipes (mnp) is a Unix Monero wallet client.
- * Copyright (C) 2019-2020
- * http://mnp4i54qnixz336alilggo4zkxgf35oj7kodkli2as4q6gpvvy5lxrad.onion
+/* Monero Named Pipes (mnp) is a Unix Monero payment processor.
+ * Copyright (C) 2019-2024
+ * https://github.com/d4ndox/mnp
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
