ac06fc2da04c8af65a138d3f5b5eab9a94df71f9
d4ndox <d4ndo@proton.me>
2024-02-21 14:02:03 +0100

    moved size_t WriteMemoryCallback from .h to .c

--- wallet.h
+++ wallet.h
@@ -1,8 +1,6 @@
 #ifndef WALLET_H
 #define WALLET_H
 
-/* defined redundant because of static */
-static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
 int wallet(const char *urlport, const char *cmd, const char *userpwd, char **answer);
 
 #endif
