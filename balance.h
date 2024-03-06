12fb16696d0b4689003bf193c946d2f2dc6467a1
d4ndox <d4ndo@proton.me>
2024-03-06 14:48:27 +0100

    Removed balance and bc_height. Not needed for the new design.

--- balance.h
+++
@@ -1,8 +0,0 @@
-#ifndef BALANCE_H
-#define BALANCE_H
-
-#include "./cjson/cJSON.h"
-
-char* balance(cJSON **reply, char *balance_fifo, char *status_balance);
-
-#endif
--- bc_height.h
+++
@@ -1,8 +0,0 @@
-#ifndef BC_HEIGHT_H
-#define BC_HEIGHT_H
-
-#include "./cjson/cJSON.h"
-
-char* blockchainheight(cJSON **reply, char *bc_height_fifo, char *status_bc_height);
-
-#endif
