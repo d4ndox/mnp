ee0ac6132ff4000690acad2bc10ecdbccfe72b4c
d4ndox <d4ndo@proton.me>
2025-08-17 11:17:52 +0200

    Removed. Not working with the new file structure.

--- example/readtx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-inotifywait -m /tmp/mywallet -e create -r |
-while read dir action file; do
-    path=$dir$file;
-    echo $path;
-    amount=$(cat $path);
-    echo "amount = "$amount;
-done
