0320af99608572b0efc897f249f990a43bf1ed34
d4ndox <d4ndo@proton.me>
2025-08-21 13:58:39 +0200

    The MIT license has been updated for the year 2025.

--- mnp.c
+++ mnp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 d4ndo@proton.me
+ * Copyright (c) 2025 d4ndo@proton.me
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
