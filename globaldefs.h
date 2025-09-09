85e3935fd85bce31977e192cddb42d58e74e6160
d4ndox <d4ndo@proton.me>
2025-09-09 10:07:55 +0200

    Increased release to 0.1.5

--- globaldefs.h
+++ globaldefs.h
@@ -7,7 +7,7 @@
 #define MAX_ADDR_SIZE   (96)
 #define MAX_PAYID_SIZE  (16)
 #define MAX_TXID_SIZE   (64)
-#define VERSION         "0.1.4"
+#define VERSION         "0.1.5"
 #define DEBUG           (0)
 #define CONFIG_FILE     ".mnp.ini"
 #define TMP_TXID_FILE   "/tmp/mnp.txid"
